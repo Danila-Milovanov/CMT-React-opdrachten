82243237085bf80a4233d67c0e3d69ae51b17ecc
Danila-Milovanov <9019853@student.zadkine.nl>
2025-10-29 11:57:24 +0100

    remove

--- opdracht-01-books/src/BookList.jsx
+++ opdracht-01-books/src/BookList.jsx
@@ -6,7 +6,7 @@ function BookList(){
         <div className="book-list">
             <h2>Amazon Best Books</h2>
             <div className="books">
-                <Book title="Harry Potter And The Sorcerers Stone" author="Joanne Rowling" image="//opdracht-01-books/public/images/book-1.png" />
+                <Book title="Harry Potter And The Sorcerers Stone" author="Joanne Rowling" image="//opdracht-01-books/public/images/book-1.pngx`" />
                 <Book title="Fantasia VI" author="Geronimo Stilton" image="//opdracht-01-books/public/images/book-2.png" />
                 <Book title="The Hunger Games" author="Suzanne Collins" image="//opdracht-01-books/public/images/book-3.png" />
             </div>
