7a18b3b335966e7fa25a01e2ffa0fbd3e3454c4b
Danila-Milovanov <9019853@student.zadkine.nl>
2025-10-31 10:03:12 +0100

    1.2 fix

--- opdracht-01-books/src/components/BookList.jsx
+++ opdracht-01-books/src/components/BookList.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import BookCounter from "./BookCounter";
+import Book from "./Book";
 
-function BookList ({books}) {
+function BookList () {
+       const [books, setBooks] = useState([
+        { id: 1, title: "Harry Potter And The Sorcerers Stone", author: "Joanne Rowling"},
+        { id: 2, title: "Fantasia VI", author: "Geronimo Stilton"},
+        { id: 3, title: "The Hunger Games", author: "Suzanne Collins"},
+    ]);
     return (
         <section>
         <BookCounter aantal={books.length} />
 
         {books.map((book) => (
-            <p key={book.id}>{book.title}</p>
+           <Book key={book.id} title={book.title} author={book.author}/>
         ))}
         </section>
     );
