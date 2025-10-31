9eb859310f7f9cce5ef90221d06dcb18c29919bc
Danila-Milovanov <9019853@student.zadkine.nl>
2025-10-31 10:53:21 +0100

    opdracht 3

--- opdracht-02-useEffect/src/App.jsx
+++ opdracht-02-useEffect/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Clock from './components/Clock';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
 
   return (
     <>
       <h1>Opdracht 2 - UseEffect</h1>
+      <WelcomeMessage />
       <Clock />
     </>
   )
